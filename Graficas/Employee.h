006fc8a8f5c7dba83be04160bc4d69be20681f84
DESKTOP-H0AOUGB\edx95 <edd.borja95@gmail.com>
2017-09-05 10:52:08 -0500

    actividad4

---
+++ Graficas/Employee.h
@@ -0,0 +1,22 @@
+#pragma once
+#include<string>
+
+
+class Employee {
+private:
+	int _id;
+	std::string _firstName;
+	std::string _lastName;
+	int _salary;
+public:
+	Employee(int id, std::string firstName, std::string lastName, int salary);
+	int GetID();
+	std::string GetFirstName();
+	std::string GetLastName();
+	std::string GetName();
+	int GetSalary();
+	void SetSalary(int salary);
+	int GetAnnualSalary();
+	int RaiseSalary(int percent);
+	std::string Print();
+};
